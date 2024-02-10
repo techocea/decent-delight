fbfb621d2c5e2c96d9a164364c69dfa0a9513fa0
techocea <dunsfordbright@gmail.com>
2024-02-11 01:17:31 +0530

    finaal

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -5,7 +5,7 @@ import {CartProvider as USCProvider} from "use-shopping-cart"
 export default function CartProvider({children}:{children:ReactNode}){
 return (
     <USCProvider 
-    node="payment"
+    mode="payment"
     cartMode="client-only"
     stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
     successUrl="http://localhost:3000/stripe/success"
